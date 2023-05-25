213536b5a52330e0b1d1618ddfb70eead47e1abd
Sean Watts <swatts@alumni.stevens.edu>
2023-05-24 23:59:16 -0400

    Removed copy button since it won't work with http

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -1,20 +1,3 @@
 window.onload = function() {
   document.getElementById("webhookUrl").value = "http://" + window.location.host + "/webhook/plex";
 };
-
-
-function copy() {
-
-  // Get the text field
-  var copyText = document.getElementById("webhookUrl");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  document.getElementById("copyBtn").innerHTML = "Copied";
-}
